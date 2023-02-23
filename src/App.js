d95b3048a68118ac081ac6fbd5710aefdd6d30f1
Sam Victor <iamvictorsam@gmail.com>
2023-02-23 17:35:40 +0100

    working on navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { About, Footer, Header, Skills, Testimonials, Work } from './container';
+import { Navbar } from './components';
+import './App.scss';
 
 function App() {
   return (
-    <h1 >
-      App
-    </h1>
+    <div className='app'>
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonials />
+      <Footer />
+    </div>
   );
 }
 
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Navbar.scss";
+import { images } from "../../constants";
+
+const Navbar = () => {
+  return (
+    <nav className="app__navbar">
+      <div className="app__navbar-logo ">
+        <img src={images.logo} alt="" />
+      </div>
+      <ul className="app__navbar-links">
+        {["home", "about", "work", "skill", "contact"].map((item) => (
+          <li key={`link-${item}`} className="app__flex p-text">
+            <div></div>
+            <a href={`##{item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
